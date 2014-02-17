31e812e414ca8972bf4283ac8dde371ea9dc296c
moljac <mcvjetko@holisticware.net>
2014-02-17 21:59:39 +0100

    MVC4 WebAPI 1 Twitter bootstrap working

---
+++ 4.0/HolisticWare.WebApp.MVC4RazorTwitterBootstrapWebAPI/NavigationRoutes/INavigationRouteFilter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web.Routing;
+
+namespace NavigationRoutes
+{
+    public interface INavigationRouteFilter
+    {
+        bool  ShouldRemove(Route navigationRoutes);
+    }
+}
