5a1ecc64ea4558d2a3927a77950b7a6521554d5e
moljac <mcvjetko@holisticware.net>
2014-02-17 14:27:13 +0100

    ASP.net MVC4, WebAPI 1 working sample for mono!

---
+++ 4.5/HolisticWare.WebApp.MVC4RazorWebAPI/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace HolisticWare.WebApp.MVC4RazorWebAPI.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
